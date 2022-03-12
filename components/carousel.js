7cd40e896d739e1a8a3b3bf4c2690e709868bc34
erj826 <erj826@gmail.com>
2022-03-12 18:33:14 -0500

    comment out carousel

--- components/carousel.js
+++ components/carousel.js
@@ -1,13 +1,13 @@
 import Image from "next/image";
 import styled from "styled-components";
 
-import img1 from "../public/assets/jonny1.jpg";
-import img2 from "../public/assets/jonny2.jpg";
-import img3 from "../public/assets/jonny3.jpg";
-import img4 from "../public/assets/jonny4.jpg";
-import img5 from "../public/assets/jonny5.jpg";
+// import img1 from "/public/assets/jonny1.jpg";
+// import img2 from "/public/assets/jonny2.jpg";
+// import img3 from "/public/assets/jonny3.jpg";
+// import img4 from "/public/assets/jonny4.jpg";
+// import img5 from "/public/assets/jonny5.jpg";
 
-const IMAGES = [img1, img2, img3, img4, img5];
+// const IMAGES = [img1, img2, img3, img4, img5];
 
 const CarouselContainer = styled.div`
   display: grid;
